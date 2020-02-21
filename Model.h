80eacd0b328279ce8beb6930c614fd6c6e485025
Robert A Alfieri <balfieri@nvidia.com>
2020-02-21 10:22:36 -0500

    TODO

--- Model.h
+++ Model.h
@@ -91,9 +91,13 @@
 //
 // To Do:
 //
-//     1) Support rest of .obj and .mtl commands and options.
+//     1) Support the .fbx binary and text formats.
+//     2) Perhaps support other formats such as UE4 and MAX.
+//     3) Test against more complex models, such as Disney Moana.
+//     4) Support more .obj and .mtl commands and options.
 //        - v: we disregard 4+ extra params
-//     2) Support the .fbx and .max binary formats.  Currently, you must find an fbx-to-obj conversion program.
+//     5) Add support for curves, patches, hair, etc.
+//     6) Add a simple ray-tracing renderer as a separate file for an example and test.
 //
 #ifndef _Model_h
 #define _Model_h
